b2ea8d5763772c86163b11f023569c6b7a43c062
Tomek <8633257+Hermanowicz@users.noreply.github.com>
2022-11-02 18:34:16 +0000

    func to list buckets in S3

--- Click_apps/demo.py
+++ Click_apps/demo.py
@@ -1,9 +1,16 @@
 """
 Demo Click app.
 """
+import json
 import click
+import boto3
 
-@click.command()
+@click.group()
+def demo():
+    pass
+
+
+@demo.command()
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
@@ -12,6 +19,13 @@ def hello(count:int, name:str):
     for x in range(count):
         click.echo(f"Hello {name.capitalize()}!")
 
+@demo.command()
+def list_buckets():
+    s3 = boto3.client("s3")
+    buckets = s3.list_buckets()["Buckets"]
+    for i in buckets:
+        print(i["Name"])
+
 
 if __name__ == '__main__':
-    hello()
+    demo()
